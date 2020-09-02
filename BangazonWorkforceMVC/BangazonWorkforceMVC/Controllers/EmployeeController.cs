7d79973f300b83589bcad38d26cb92463d13695b
Ashon Woodbury <a.woodbury01@gmail.com>
2020-09-02 11:49:28 -0400

    finished ticket 1

--- BangazonWorkforceMVC/BangazonWorkforceMVC/Controllers/EmployeeController.cs
+++ BangazonWorkforceMVC/BangazonWorkforceMVC/Controllers/EmployeeController.cs
@@ -1,18 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BangazonAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace BangazonWorkforceMVC.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
         // GET: EmployeeController
         public ActionResult Index()
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+            SELECT e.Id,
+                e.FirstName,
+                e.LastName,
+                e.DepartmentId,
+                e.isSuperVisor
+            FROM Employee e
+        ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Employee> employees = new List<Employee>();
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                            isSupervisor = reader.GetBoolean(reader.GetOrdinal("isSupervisor"))
+                        };
+
+                        employees.Add(employee);
+                    }
+
+                    reader.Close();
+
+                    return View(employees);
+                }
+            }
         }
 
         // GET: EmployeeController/Details/5
--- BangazonWorkforceMVC/BangazonWorkforceMVC/Models/Employee.cs
+++ BangazonWorkforceMVC/BangazonWorkforceMVC/Models/Employee.cs
@@ -1,13 +1,20 @@
-﻿namespace BangazonAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BangazonAPI.Models
 {
     public class Employee
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
+
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
 
         public string LastName { get; set; }
 
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
 
         public Department department { get; set; }
