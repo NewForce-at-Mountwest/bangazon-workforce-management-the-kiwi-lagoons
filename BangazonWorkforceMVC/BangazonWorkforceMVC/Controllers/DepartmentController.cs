278c469031f93bcf1144a35bd9e99339f5c991ea
StephenAvila2020 <stephen.r.avila@gmail.com>
2020-09-03 14:33:18 -0400

    added create function

--- BangazonWorkforceMVC/BangazonWorkforceMVC/Controllers/DepartmentController.cs
+++ BangazonWorkforceMVC/BangazonWorkforceMVC/Controllers/DepartmentController.cs
@@ -4,8 +4,10 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using BangazonAPI.Models;
+using BangazonWorkforceMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 
 namespace BangazonWorkforceMVC.Controllers
@@ -77,17 +79,73 @@ namespace BangazonWorkforceMVC.Controllers
         // GET: DepartmentController/Create
         public ActionResult Create()
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    // Select all the cohorts
+                    cmd.CommandText = @"SELECT Department.Id, Department.Name, Department.Budget FROM Department";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Create a new instance of our view model
+                    DepartmentViewModel viewModel = new DepartmentViewModel();
+                    while (reader.Read())
+                    {
+                        // Map the raw data to our cohort model
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                        };
+
+                        // Use the info to build our SelectListItem
+                        SelectListItem departmentOptionTag = new SelectListItem()
+                        {
+                            Text = department.Name,
+                            Value = department.Id.ToString()
+                        };
+
+                        // Add the select list item to our list of dropdown options
+                        viewModel.departments.Add(departmentOptionTag);
+
+                    }
+
+                    reader.Close();
+
+
+                    // send it all to the view
+                    return View(viewModel);
+                }
+            }
         }
 
         // POST: DepartmentController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(DepartmentViewModel viewModel)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Department
+                ( Name, Budget )
+                VALUES
+                ( @Name, @Budget )";
+                        cmd.Parameters.Add(new SqlParameter("@Name", viewModel.department.Name));
+                        cmd.Parameters.Add(new SqlParameter("@Budget", viewModel.department.Budget));
+                        cmd.ExecuteNonQuery();
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
             }
             catch
             {
